0eabc073d91e176b2ce5abdb81465ca454cb4052
Wesley Gonçalves <dev@wesleygoncalves.com>
2026-04-20 02:48:06 -0300

    fix: missing export Products

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ export * from "./live_chat";
 export * from "./health";
 export * from "./ticket";
 export * from "./companies";
+export * from "./products";
 
 export { config } from "./config";
 export type { LibraryConfig } from "./config";
